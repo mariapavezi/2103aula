a44b9342fb2e3d528bee25e36f816ce1485b6055
mariapavezi <maria.luisa.pavezi.08@gmail.com>
2025-03-24 19:47:37 -0300

    aula 21-03

---
+++ script.js
@@ -0,0 +1,10 @@
+function ligar(){
+   let lampada = document.getElementById
+   ("lampada");
+   /*a variavel lampada será a tag img */
+   lampada.src = "img/ligada.jpg";
+}
+
+function desligar(){   
+    document.getElementById("lampada").src = "img/desl.avif";
+}
